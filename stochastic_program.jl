f24575cab1cb0aa0b3ec27f9e3effc679696d8e0
marloanzarut123 <marloanzarut@gmail.com>
2024-03-05 16:31:28 +0100

    Update stochastic_program.jl

--- stochastic_program.jl
+++ stochastic_program.jl
@@ -9,3 +9,23 @@
 #  generate 1000 equally probable scenarios using sample next
 #  (given the initial prices), reduce them to N representative ones with appropriate probabilities, and use those to
 #  solve a 2-stage stochastic program.
+
+# Deliverable 1.d
+
+using Gurobi
+using JuMP
+using Printf
+using Pkg
+using DataFrames
+using CSV
+using Distributions  # Adding Distributions package
+
+Pkg.add("Distributions")
+
+# import data from 
+include("/Users/marloanzarut/Downloads/decisionmaking_under_uncertainty/V2_02435_two_stage_problem_data.jl")
+include("/Users/marloanzarut/Downloads/decisionmaking_under_uncertainty/V2_price_process.jl")
+
+
+function make_schocastic_here_and_now_decision(prices_day_one, decision)
+    
